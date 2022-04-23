5694895a5ced66b7506de3c1ad53ca9f9e070ad0
David Doria <dvd841@gmail.com>
2022-04-23 23:55:49 +0100

    DB Struture and Population

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,45 +1,189 @@
-create table bookings
-(
-    id int not null identity auto_increment,
-    date smalldatetime,
-    num_persons int,
-    id_zone int,
-    food_order bit null,
-    id_order int null,
-    primary key (id)
-);
-create table users
-(
-    id int not null identity auto_increment,
-    username varchar(255),
-    email varchar(255),
-    phone varchar(9),
-    first_name varchar(255),
-    last_name varchar(255),
-    birthday smalldatetime,
-    age int,
-    primary key(id)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Tempo de geração: 23-Abr-2022 às 20:47
+-- Versão do servidor: 8.0.28
+-- versão do PHP: 8.0.15
 
-create table zones
-(
-    id_zone int not null identity auto_increment,
-    zone_description varchar(255),
-    primary key (id_zone)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-create table menu
-(
-    id int not null identity auto_increment,
-    description varchar(max),
-    primary key (id)
-);
 
-create table order
-(
-    id int not null identity auto_increment,
-    id_order int not null,
-    id_menu int,
-    quantity int,
-    primary key (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `shisha_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int NOT NULL,
+  `date` date DEFAULT NULL,
+  `num_persons` int DEFAULT NULL,
+  `id_zone` int DEFAULT NULL,
+  `order_food` tinyint(1) DEFAULT NULL,
+  `id_order` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientOrder`
+--
+
+CREATE TABLE `clientOrder` (
+  `id` int NOT NULL,
+  `id_order` int DEFAULT NULL,
+  `id_menu` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `menu`
+--
+
+INSERT INTO `menu` (`id`, `description`) VALUES
+(1, 'Francesinha'),
+(2, 'Taquitos'),
+(3, 'Tacos'),
+(4, 'Nachos');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(9) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `age` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `phone`, `first_name`, `last_name`, `birthday`, `age`) VALUES
+(1, 'davidgdoria', 'dvd841@gmail.com', '967387059', 'David', 'Doria', '1990-01-14', 32),
+(2, 'brunompinto', 'brunompinto@gmail.com', '961234567', 'Bruno', 'Pinto', '1989-09-03', 33);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `zones`
+--
+
+CREATE TABLE `zones` (
+  `id_zone` int NOT NULL,
+  `zone_description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `zones`
+--
+
+INSERT INTO `zones` (`id_zone`, `zone_description`) VALUES
+(1, 'Shisha'),
+(2, 'Sala Jantar'),
+(3, 'Jantar / Shisha');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `clientOrder`
+--
+ALTER TABLE `clientOrder`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `zones`
+--
+ALTER TABLE `zones`
+  ADD PRIMARY KEY (`id_zone`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `clientOrder`
+--
+ALTER TABLE `clientOrder`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `zones`
+--
+ALTER TABLE `zones`
+  MODIFY `id_zone` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
